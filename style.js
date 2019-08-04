ea2d78a892b70f213ed4dea8716f45a5aa3aee86
CHRISTOPHER HEISER <heiser@nau.edu>
2019-08-03 22:56:02 -0700

    Interview Page

---
+++ style.js
@@ -0,0 +1,34 @@
+var scroll = window.requestAnimationFrame ||
+function(callback){ window.setTimeout(callback, 1000/60)};
+var elements = document.querySelectorAll('.scroll'); 
+
+function loop() {
+
+Array.prototype.forEach.call(elements, function(element){
+    if (inView(element)) {
+        console.log("Adding");
+        element.classList.add('show');
+    }
+});
+
+scroll(loop);
+}
+
+loop();
+
+function inView(el) {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+        (rect.top <= 0
+        && rect.bottom >= 0)
+        ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
+        (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+}
